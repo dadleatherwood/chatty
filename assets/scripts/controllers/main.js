cc67ee75c8bf8890afe2140bc7af4cca4d388106
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-06-13 16:41:40 -0500

    completed project with black diamond

--- assets/scripts/controllers/main.js
+++ assets/scripts/controllers/main.js
@@ -2,17 +2,32 @@
 
 angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
+    console.log("Hi")
     messageService.getMessages().then(function ( response ) {
       $scope.messages = response.data.messages;
+      console.log($scope.messages)
     });
 
     $scope.addMessage = function ( message ) {
       if (message) {
-        messageService.addMessage(message).then(function ( response ) {
+        var body = {
+          message: message,
+          username: $scope.username
+        }
+        messageService.addMessage(body).then(function ( response ) {
           $scope.messages = response.data.messages;
         });
       }
     };
+
+    //myCode
+
+
+    $scope.addUsername = function (username) {
+      if (username) {
+          $scope.username = username
+      }
+    }
 
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var messages = []
+var usernames = []
 
 
 app.use(express.static('assets'))
@@ -12,11 +13,16 @@ app.get('/messages', function(req, res, next){
 })
 
 app.post('/messages', function (req, res, next) {
-  messages.push({ message: req.body.message, time: new Date() });
+  messages.push({ message: req.body.message, username: req.body.username, time: new Date() });
   res.status(200).json({messages: messages})
   console.log(req.body)
   console.log({messages: messages})
 })
+
+
+
+
+
 
 app.listen(3000, function(){
   console.log("I'm not Listening!")
